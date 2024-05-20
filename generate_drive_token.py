8225ef322e95b9f84b531ccc5c598fbd38eb9a52
Loki <121497865+Jitroy45@users.noreply.github.com>
2024-05-20 18:54:12 +0530

    Update generate_drive_token.py

--- generate_drive_token.py
+++ generate_drive_token.py
@@ -6,20 +6,29 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 credentials = None
 __G_DRIVE_TOKEN_FILE = "token.pickle"
 __OAUTH_SCOPE = ["https://www.googleapis.com/auth/drive"]
-if os.path.exists(__G_DRIVE_TOKEN_FILE):
-    with open(__G_DRIVE_TOKEN_FILE, "rb") as f:
-        credentials = pickle.load(f)
-        if (
-            (credentials is None or not credentials.valid)
-            and credentials
-            and credentials.expired
-            and credentials.refresh_token
-        ):
-            credentials.refresh(Request())
-else:
-    flow = InstalledAppFlow.from_client_secrets_file("credentials.json", __OAUTH_SCOPE)
-    credentials = flow.run_local_server(port=0, open_browser=False)
+def get_credentials():
+    credentials = None
 
-# Save the credentials for the next run
-with open(__G_DRIVE_TOKEN_FILE, "wb") as token:
-    pickle.dump(credentials, token)
+    if os.path.exists(__G_DRIVE_TOKEN_FILE):
+        with open(__G_DRIVE_TOKEN_FILE, "rb") as f:
+            credentials = pickle.load(f)
+
+    if (
+        not credentials
+        or not credentials.valid
+        or credentials.expired
+    ):
+        if credentials and credentials.refresh_token:
+            credentials.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", __OAUTH_SCOPE)
+            credentials = flow.run_local_server(port=0)
+
+        # Save the credentials for the next run
+        with open(__G_DRIVE_TOKEN_FILE, "wb") as token:
+            pickle.dump(credentials, token)
+
+    return credentials
+
+# Get the credentials
+credentials = get_credentials()
